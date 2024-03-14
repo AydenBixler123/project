d7125e17360253828d735d445115542dbf2396e3
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-13 21:19:16 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,15 +1,16 @@
-import streamlit as st
-import pandas as pd
+try:
+    import streamlit as st
+    import pandas as pd
 
-st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
+    st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
 
-uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
+    uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
 
-df = pd.read_csv(uploadedfile)
+    df = pd.read_csv(uploadedfile)
 
-df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
+    df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
 
-for ele in df:
-    df.dropna(axis=0,inplace=True)
+    for ele in df:
+        df.dropna(axis=0,inplace=True)
 
-st.write(df)
+    st.write(df)
