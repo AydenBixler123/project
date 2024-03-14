d3664f496537cf3796eacdcfa45ac1d86eee090f
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-13 21:13:21 -0400

    Update project.py

--- project.py
+++ project.py
@@ -5,16 +5,11 @@ st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
 
 uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
 
-df = 0
-
 df = pd.read_csv(uploadedfile)
 
 df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
 
 for ele in df:
     df.dropna(axis=0,inplace=True)
-    
-try:
-    st.write(df)
-except ValueError:
-        st.write("Please insert the Titanic-Dataset.csv file")
+
+#st.write(df)
