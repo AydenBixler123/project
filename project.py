bbc98b8746314c1746ef9982dd785456e06d1b2e
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-13 20:46:18 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
+
+uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
+
+df = pd.read_csv(uploadedfile)
+
+df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
+
+for ele in df:
+    df.dropna(axis=0,inplace=True)
+
+missing_vals = df.isnull().sum()
+
+st.write(df)
