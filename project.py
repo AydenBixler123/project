6f70f204ce739d4c969051871f25ed287460ea54
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-13 21:10:58 -0400

    Update project.py

--- project.py
+++ project.py
@@ -13,9 +13,8 @@ df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
 
 for ele in df:
     df.dropna(axis=0,inplace=True)
-
-While True:
-    try:
-        st.write(df)
-    except ValueError:
+    
+try:
+    st.write(df)
+except ValueError:
         st.write("Please insert the Titanic-Dataset.csv file")
