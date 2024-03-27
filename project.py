10c9d1fb3d1d50255c40b1ba546cf4d3a608cb78
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-27 19:53:49 -0400

    Update project.py

--- project.py
+++ project.py
@@ -2,19 +2,18 @@ try:
 
     import streamlit as st
     import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.feature_selection import VarianceThreshold
-    from sklearn.pipeline import Pipeline
-    from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer, MaxAbsScaler, LabelEncoder
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.ensemble import BaggingClassifier
-    from sklearn.tree import DecisionTreeClassifier
+    #from sklearn.model_selection import train_test_split
+    #from sklearn.neighbors import KNeighborsClassifier
+    #from sklearn.feature_selection import VarianceThreshold
+    #from sklearn.pipeline import Pipeline
+    #from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer, MaxAbsScaler, LabelEncoder
+    #from sklearn.ensemble import RandomForestClassifier
+    #from sklearn.ensemble import BaggingClassifier
+    #from sklearn.tree import DecisionTreeClassifier
     
     st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
-    
-    #st.write("Please download the Titanic-Dataset.csv file from: www.kaggle.com/datasets/yasserh/titanic-dataset") 
-    #uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
+
+    uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
 
     df = pd.read_csv(uploadedfile)
 
