9ab4c5a3e3077f9e3c1107c1c3e10f89396f3b2f
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-27 19:39:34 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,18 @@
 try:
     import streamlit as st
     import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.feature_selection import VarianceThreshold
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer, MaxAbsScaler, LabelEncoder
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import BaggingClassifier
+    from sklearn.tree import DecisionTreeClassifier
 
     st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
-
+    
+    st.download_button("Please download the Titanic-Dataset.csv file", https://www.kaggle.com/datasets/yasserh/titanic-dataset/download?datasetVersionNumber=1) 
     uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
 
     df = pd.read_csv(uploadedfile)
@@ -14,6 +23,8 @@ try:
         df.dropna(axis=0,inplace=True)
 
     st.write(df)
+
+    
 
 except ValueError as ve:
     print("")
