247401d1e424e74991caedf453ae925df1e4ec2f
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-27 19:46:40 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
 import streamlit as st
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.feature_selection import VarianceThreshold
-    from sklearn.pipeline import Pipeline
-    from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer, MaxAbsScaler, LabelEncoder
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.ensemble import BaggingClassifier
-    from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer, MaxAbsScaler, LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 try:
 
