784f045676372d8f1197f39d33cc25716f2312fe
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-03-27 19:54:03 -0400

    Update project.py

--- project.py
+++ project.py
@@ -13,6 +13,7 @@ try:
     
     st.title("CIS 335 Project By: Ayden Bixler and Matthew Janatello")
 
+    st.write("Please download the Titanic-Dataset.csv file from: www.kaggle.com/datasets/yasserh/titanic-dataset") 
     uploadedfile = st.file_uploader("Please insert the Titanic-Dataset.csv file")
 
     df = pd.read_csv(uploadedfile)
