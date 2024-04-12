25e3c1bd5aadc9710816efc5f83c5300cbc6a3f7
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-04-12 13:17:31 -0400

    Update project.py

--- project.py
+++ project.py
@@ -47,8 +47,10 @@ try:
     )
 
     if "Decision Tree" in selected_classifier:
-     min_samples_split = st.sidebar.slider('Choose a value for the min samples split parameter.', 0, 10, 2)
+     min_samples_split = st.sidebar.slider('Choose a value for the min samples split parameter.', 1, 10, 2)
      max_depth = st.sidebar.slider('Choose a value for the max depth parameter.', 1, 5, 1)
+    elif "SVM" in selected_classifier:
+     parameter = st.sidebar.slider('Choose a value for the  parameter.', 1, 1, 1)
     else:
      pass
 	
