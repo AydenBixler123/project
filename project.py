6d570ca502f6937d5d13727d5e41e779e9e7e8a1
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-04-14 11:39:56 -0400

    Update project.py

--- project.py
+++ project.py
@@ -163,6 +163,7 @@ try:
      grid = GridSearchCV(pipe, parameters, cv=2).fit(xtrain_normalized, ytrain)
 	    
      pass
+	    
      with st.form('form1'):
       st.write('this is a form')
       st.form_submit_button('Press to Calculate')
