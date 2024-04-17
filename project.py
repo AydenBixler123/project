57942124e31dbe512497572d0a8bf43c3cd90be0
AydenBixler123 <163363544+AydenBixler123@users.noreply.github.com>
2024-04-17 19:52:57 -0400

    Update project.py

--- project.py
+++ project.py
@@ -26,7 +26,7 @@ try:
 
     df = pd.read_csv(uploadedfile)
 
-    df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name'], axis=1)
+    df = df.drop(['PassengerId', 'Embarked', 'Cabin', 'Ticket', 'Name', 'Sex'], axis=1)
 
     for ele in df:
         df.dropna(axis=0,inplace=True)
